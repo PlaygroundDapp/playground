8bcbb9d08c628a689671cc3a8498c3335d4093a8
dinos <constantine@seatedapp.io>
2022-01-22 14:16:41 -0500

    added tests

--- test/Playground.js
+++ test/Playground.js
@@ -3,21 +3,28 @@ const { ethers } = require("hardhat");
 
 describe("Playground Contract", () => {
   let playground;
-  let owner, user;
+  let owner;
+  let shareholders = [];
 
   beforeEach(async () => {
     const Playground = await ethers.getContractFactory("Playground");
     playground = await Playground.deploy();
     await playground.deployed();
 
-    [owner, user] = await ethers.getSigners();
+    const signers = await ethers.getSigners();
+    owner = signers[0];
+    shareholders.push([signers[1], 50]);
+    shareholders.push([signers[2], 20]);
+    shareholders.push([signers[3], 10]);
+    shareholders.push([signers[4], 10]);
+    shareholders.push([signers[5], 10]);
     
     const projectId = playground.createProject();
   });
 
   describe("mint", function () {
     it("createProject to return project id", async () => {
-      
+      playground
     });
   });
 });
