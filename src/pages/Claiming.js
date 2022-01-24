6e310530b1650aa23a22d4cc5bb46e9dbd7e4585
Jungho Bang <me@bangtoven.com>
2022-01-23 17:08:41 -0800

    to confuse git
    to confuse git 2

--- src/pages/Claiming.js
+++ src/pages/Claiming.js
@@ -1,53 +1,25 @@
 import { useState } from "react";
-
-import { useWeb3Context } from "../hooks/useWeb3Context";
-import { usePlayground } from "../hooks/usePlayground";
-import address from "../abis/contract-address.json";
+import { claim, getTokens } from "../../utils/common"
+import { useWeb3Context } from "../../hooks/useWeb3Context";
 
 
-export default function Claim() {
+export default function Claiming() {
   const { account } = useWeb3Context();
   const [tokens, setTokens] = useState([]);
-  
-  // TODO: get contract address from user input
-  const contract = usePlayground(address.PlaygroundContract);
 
-  const claimEarnings = async (tokenId) => {
-    // copied `export const claim = async (tokenId) => {` from utils.common.js
-    try {
-      // const contract = getContract();
-      const txn = await contract.claim(tokenId);
-      console.log(txn);
-      await txn.wait();
-    } catch (error) {
-      console.log(error);
-    }
+  const claimEarnings = (tokenId) => {
+    claim(tokenId);
   }
 
   const getTokensForUser = async () => {
-    // copied `export const getTokens = async (account) => {` from utils.common.js
-    const tokens = [];
-    try {
-        // const contract = getContract();
-        for (let i = 0; true; i++) {
-            let token = await contract.tokenOfOwnerByIndex(account, i);
-            let share = await contract.shares(token.toString());
-            tokens.push({
-                tokenId: token.toString(),
-                share: share.toString()
-            });
-        }
-    } catch (error) {
-        console.log(tokens);
-    }
-    setTokens(tokens);
+    setTokens(await getTokens(account));
     console.log(tokens);
   }
 
   return (
     <div className="container mx-auto">  
         <h1 className="text-xl mt-16"> Claim your earnings</h1>
-        
+
         <div className="mt-8">
           <button className="btn-primary btn" onClick={() => getTokensForUser()}> Get Tokens</button>
         </div>
