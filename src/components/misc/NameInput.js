db2c8dc84713182bf33d4adb9543a8cada976df9
Tobi <37726406+akinwol@users.noreply.github.com>
2022-01-23 21:55:02 -0500

    Mint fixing (#20)
    * fixed deposit
    
    * updated nav and mint page
    
    Co-authored-by: Olutobi  Akinwumi <tobicodes@Olutobis-MacBook-Pro.local>

--- src/components/misc/NameInput.js
+++ src/components/misc/NameInput.js
@@ -1,9 +1,9 @@
-export default function NameInput({ displayName, name, value, placeholder, onChange, buttonName, buttonClick}) {
+export default function NameInput({inputs = [], type= "text",disabled, displayName, name, value, placeholder, onChange, buttonName, buttonClick}) {
     return (
         <div className="flex items-center gap-6">
-                <div className=""> {displayName}</div>
-                <input type="text" placeholder={placeholder} className="input input-bordered"  value={value} name={name} onChange={onChange}/>
-                {buttonName && <button className="btn btn-neutral rounded-full btn-medium" onClick={buttonClick}> Add </button>}
+               {displayName && <div className=""> {displayName}</div>} 
+                <input type={type} placeholder={placeholder} className="input input-bordered"  value={value} name={name} onChange={onChange}/>
+                {buttonName && <button disabled={disabled} className="btn btn-neutral rounded-full btn-medium" onClick={buttonClick}> Add </button>}
         </div>
     )
 }
