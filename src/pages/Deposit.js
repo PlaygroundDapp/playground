84dd4af13e45271fff492bba6c6553298776bec9
Jungho Bang <me@bangtoven.com>
2022-01-25 21:16:13 -0800

    deposit

--- src/pages/Deposit.js
+++ src/pages/Deposit.js
@@ -3,36 +3,42 @@ import { ethers } from "ethers";
 
 import { useWeb3Context } from "../hooks/useWeb3Context";
 import { useProjectContract } from "../hooks/useContract";
-import address from "../abis/contract-address.json";
+import SharesTable from "../components/misc/SharesTable";
 
 export default function Deposit() {
-  const { account, provider, isActive, connectWallet } = useWeb3Context();
-
-  const [myETHBalance, setMyETHBalance] = useState(0);
-  const [totalDepositedAmount, setTotalDepositedAmount] = useState("--");
-
-  // TODO: get contract address from user input
-  const contract = useProjectContract(address.PlaygroundContract);
-
-  useEffect(() => {
-    if (!contract || !provider) {
-      return;
-    }
-    provider.once("block", () => {
-      contract.on("Deposit", () => {
-        window.alert("Successfully Deposited");
-      });
-    });
-  }, [contract, provider]);
+  const { provider, contractAddress } = useWeb3Context();
+  const [currentContractAddress, setCurrentContractAddress] = useState(
+    contractAddress.address
+  );
+  const contract = useProjectContract(currentContractAddress);
+  const [shareholders, setShareholders] = useState([]);
 
   useEffect(() => {
-    async function fetchETHBalance() {
-      const balanceBN = await provider.getBalance(account);
-      const balance = ethers.utils.formatEther(balanceBN);
-      setMyETHBalance(balance);
-    }
-    fetchETHBalance();
-  }, [account, provider]);
+    if (!contract) return;
+
+    const fetchShareholderInfo = async () => {
+      const shareholders = [];
+      try {
+        const totalSupply = await contract.totalSupply();
+        for (let i = 0; i < totalSupply.toNumber(); i++) {
+          let tokenId = (await contract.tokenByIndex(i)).toString();
+          let share = await contract.shares(tokenId);
+          let owner = await contract.ownerOf(tokenId);
+          shareholders.push({
+            tokenId: tokenId,
+            tokenOwner: owner.toString(),
+            tokenShare: share.toString(),
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      setShareholders(shareholders);
+    };
+
+    fetchShareholderInfo();
+  }, [contract]);
 
   async function deposit() {
     const amountString = prompt("how much do you want to deposit?");
@@ -43,36 +49,41 @@ export default function Deposit() {
     console.log(response);
   }
 
-  async function reloadTotalDepositAmount() {
-    const totalBN = await contract.totalDepositedAmount();
-    const total = ethers.utils.formatEther(totalBN);
-    setTotalDepositedAmount(total);
-  }
+  const handleAddressChange = (e) => {
+    const input = e.target.value;
+    setCurrentContractAddress(input && input.length ? input : null);
+  };
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-xl mt-16">Deposit </h1>
-      {!isActive ? (
-        <button className="btn-primary btn" onClick={connectWallet}>
-          Connect Wallet
-        </button>
-      ) : (
-        <div className="flex flex-col">
-          <label>My address: {account}</label>
-          <label>My ETH balance: {myETHBalance}</label>
-          <label>contract.totalDepositedAmount: {totalDepositedAmount}</label>
-          <button className="btn-primary btn" onClick={deposit}>
-            Deposit
-          </button>
-          (might take some time for the deposit tx to get mined)
-          <button
-            className="btn-primary btn"
-            onClick={reloadTotalDepositAmount}
-          >
-            Check total deposit
-          </button>
+      <h1 className="text-4xl mt-16"> Deposit</h1>
+      <div className="mt-8 mb-4">
+        <input
+          type="text"
+          placeholder="Contract address"
+          className="input input-bordered w-full"
+          value={contractAddress}
+          onChange={handleAddressChange}
+        />
+      </div>
+      <div className="border-2 p-6 mt-6">
+        <h1 className="text-l font-bold"> Shareholders </h1>
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <SharesTable
+                shares={shareholders}
+                contractLoaded={contract !== null}
+              />
+            </div>
+          </div>
         </div>
-      )}
+      </div>
+      <div className="mt-8 mb-4">
+        <button className="btn-primary btn" onClick={deposit}>
+          Deposit
+        </button>
+      </div>
     </div>
   );
 }
