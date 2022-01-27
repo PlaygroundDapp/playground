9b2b9338fbc669a8eac636644bc578a76ebd0b94
Charles Naut <cpn585@gmail.com>
2022-01-27 09:51:15 -0500

    Claim cleanup (#31)
    * Commit fixes for merge conflicts

--- src/pages/Claim.js
+++ src/pages/Claim.js
@@ -92,7 +92,7 @@ export default function Claim() {
 
 
         <div className="mt-8 mb-4">
-          <input type="text" placeholder="Contract address" className="input input-bordered w-full" value={currentContractAddress} onChange={handleAddressChange}/>
+          Enter contract address: <input type="text" placeholder="Contract address" className="input input-bordered w-24" value={currentContractAddress} onChange={handleAddressChange}/>
         </div>
 
         <ClaimEarnings address={currentContractAddress} />
